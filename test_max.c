41cf38ffaada74a9d555cbcf5ab3b4724dd11392
malavikasajeev <85017062+malavikasajeev@users.noreply.github.com>
2021-06-19 02:33:00 +0530

    Update test_max.c

--- test_max.c
+++ test_max.c
@@ -2,7 +2,16 @@
 #include <assert.h>
 
 
-int max(int [], int);
+int max(int arr[], int n)
+  {
+    int i;
+    int max = arr[0];
+    for (i = 1; i < n; i++)
+        if (arr[i] > max)
+            max = arr[i];
+ 
+    return max;
+}
 
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
